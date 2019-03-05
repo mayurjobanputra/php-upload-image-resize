f9fb73bb41fea1136d3337da7d589a1a44ab5511
joelau <joe.laucw71@gmail.com>
2019-03-05 15:40:31 +0800

    add upload.php edit sample.php

--- UploadResizeImage/sample.php
+++ UploadResizeImage/sample.php
@@ -1,19 +1,3 @@
-<?php
-	if (isset($_POST["author"])) {
-		include_once("UploadResizeImage.php");
-		$file = $_FILES["screenshot"];
-		$resize = new UploadResizeImage($file);
-		$resize->setProperty("width", 300);
-		$resize->setProperty("height", 200);
-		$resize->setProperty("model", "large");
-		$resize->execute();
-		$resize->save("testing.jpg");
-		header('Content-Type: image/jpeg');
-		imagejpeg($resize->get_resize_image());
-		die();
-	}
-?>
-
 <!DOCTYPE HTML>
 <html>
 <head>
