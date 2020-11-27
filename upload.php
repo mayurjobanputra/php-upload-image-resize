ed18311a8a9c7e2c5c45c7ee2971ca551043389d
mayurjobanputra <mayur@glvp.com>
2020-11-26 17:39:28 -0800

    Update upload.php
    Moves images uploaded to an .htaccess protected upload folder

--- upload.php
+++ upload.php
@@ -2,12 +2,13 @@
   $generatedate = new Datetime("now");
   $uniqueimage = $generatedate->format('U') . ".jpg"; 
   //echo $uniqueimage;
+  $uniqueimage = "images/" . $uniqueimage;
   
   include_once("uploadresizeimage.php");
   $resize = new UploadResizeImage($_FILES["upload"]);
   $resize->targetWidth = 1200;
   $resize->targetHeight = 1200;
-  $resize->mode = "contain";
+  $resize->mode = "cover";
   $resize->save($uniqueimage); //eg. upload/photo.jpg
   
   header('Location:' . $uniqueimage);
